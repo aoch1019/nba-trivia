00f91841dc190d069a62d570cc70a5f7769d0070
Andrew Och <andrewoch95@gmail.com>
2018-10-30 18:08:22 -0400

    Added a previous players table

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Welcome from './Welcome'
+import { Header, Container, Image } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Container textAlign='center'>
         <div>
           <header>
-            <img src='https://i0.wp.com/meebily.com/wp-content/uploads/2018/07/NBA-Trivia-questions-answers-quiz-game.jpeg?fit=600%2C400&ssl=1' style={{width: '180px', height: '150px'}} className="App-logo" alt="logo" />
+            <Image src='https://i0.wp.com/meebily.com/wp-content/uploads/2018/07/NBA-Trivia-questions-answers-quiz-game.jpeg?fit=600%2C400&ssl=1' circular centered style={{width: '180px', height: '150px'}} className="App-logo" alt="logo" />
             <br></br><br></br>
-            <p>
+            <Header as='h2'>
               Welcome to NBA Player Trivia -- College Edition
-            </p>
+            </Header>
           </header>
         </div>
         < Welcome />
-      </React.Fragment>
+    </Container>
     )
   }
 }
--- src/Gameplay.js
+++ src/Gameplay.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import playersHash from './data'
+import { Button, Header, Label, Segment } from 'semantic-ui-react'
 
 class Gameplay extends Component {
 
@@ -7,7 +8,8 @@ class Gameplay extends Component {
     allPlayers: null,
     currPlayer: null,
     currInput: "",
-    showAnswer: false
+    showAnswer: false,
+    completedPlayers: []
   }
 
   componentDidMount(){
@@ -23,7 +25,7 @@ class Gameplay extends Component {
     while(player === null){
       let randomNum = Math.floor(Math.random() * this.state.allPlayers.length)
       let currPlayer = this.state.allPlayers[randomNum]
-      if(currPlayer.collegeName !== "" && !currPlayer.collegeName.includes(',') && !currPlayer.collegeName.includes('HS') && currPlayer !== this.state.currPlayer){
+      if(currPlayer.collegeName !== "" && !currPlayer.collegeName.includes(',') && !currPlayer.collegeName.includes('HS') && !currPlayer.collegeName.includes('Spain') && !currPlayer.collegeName.includes('H.S.') && !this.state.completedPlayers.includes(currPlayer) && !currPlayer.collegeName.includes('Greece')){
         player = currPlayer
       }
     }
@@ -43,20 +45,56 @@ class Gameplay extends Component {
   render(){
     return(
       <React.Fragment>
+        <br></br>
         {!!this.state.currPlayer &&
-          <React.Fragment>
-            <div>What college did {this.state.currPlayer.firstName} {this.state.currPlayer.lastName} go to?</div>
+          <Segment raised textAlign='center'>
+            <Header as='h4'>What college did {this.state.currPlayer.firstName} {this.state.currPlayer.lastName} go to?</Header>
             {!this.state.showAnswer
             ?
-            <button onClick={() => this.setState({ showAnswer: true })}>View Answer</button>
+            <Button onClick={() => this.setState({ showAnswer: true })}>View Answer</Button>
             :
             <React.Fragment>
-              <button onClick={() => {this.getRandomPlayer(); this.setState({ showAnswer: false }) }}>Next</button>
-              <div>{this.state.currPlayer.collegeName}</div>              
+              <Button onClick={() => {this.setState({ showAnswer: false, completedPlayers: [...this.state.completedPlayers, this.state.currPlayer] }); this.getRandomPlayer()}}>Next</Button>
+              <br></br><br></br>
+              <Label color='blue'><Header as='h4' color='yellow'>{this.state.currPlayer.collegeName}</Header></Label>
             </React.Fragment>
             }
-        </React.Fragment>
+        </Segment>
         }
+
+        <Header as='h3'>Previous Players</Header>
+          <table className="ui large celled striped padded table">
+                <tbody>
+                  <tr className="center aligned">
+                    <th>
+                      <h3 className="ui left aligned header">
+                        Player
+                      </h3>
+                    </th>
+                    <th>
+                      <h3 className="ui center aligned header">
+                        College
+                      </h3>
+                    </th>
+                  </tr>
+
+                  {this.state.completedPlayers.reverse().map((player, idx) => {
+                    return <tr key={idx} className="center aligned">
+                            <th>
+                              <h5 className="ui left aligned header">
+                                {player.firstName} {player.lastName}
+                              </h5>
+                            </th>
+                            <th>
+                              <h5 className="ui center aligned header">
+                                {player.collegeName}
+                              </h5>
+                            </th>
+                          </tr>
+                  })}
+
+                </tbody>
+              </table>
 
       </React.Fragment>
     )
